14b6e7eb6cc2a654eef4950c6efb71d1ea40d758
Stênio Melo <steniomelo@MacBook-Pro-FCX.local>
2023-12-02 15:45:27 -0300

    Trying the old code updated

--- api/screenshot.js
+++ api/screenshot.js
@@ -1,71 +1,192 @@
-const chromium = require('@sparticuz/chromium-min');
-const puppeteer = require('puppeteer-core');
-let _page;
+// Shout outs to the following repositories:
 
-async function getBrowser() {
-  // local development is broken for this 👇
-  // but it works in vercel so I'm not gonna touch it
-  return puppeteer.launch({
-    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(
-      `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
-    ),
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
-}
+// https://github.com/vercel/og-image
+// https://github.com/ireade/netlify-puppeteer-screenshot-demo
 
-async function getPage() {
-  if (_page) return _page;
+// The maximum execution timeout is 10
+// seconds when deployed on a Personal Account (Hobby plan).
+// For Teams, the execution timeout is 60 seconds (Pro plan)
+// or 900 seconds (Enterprise plan).
 
-  const browser = await getBrowser();
-  _page = await browser.newPage();
-  return _page;
-}
+const puppeteer = require("puppeteer-core");
+const chrome = require('@sparticuz/chromium-min');
 
-function checkUrl(string) {
-  var url = '';
-  try {
-    url = new URL(string);
-  } catch (error) {
-    return false;
+/** The code below determines the executable location for Chrome to
+ * start up and take the screenshot when running a local development environment.
+ *
+ * If the code is running on Windows, find chrome.exe in the default location.
+ * If the code is running on Linux, find the Chrome installation in the default location.
+ * If the code is running on MacOS, find the Chrome installation in the default location.
+ * You may need to update this code when running it locally depending on the location of
+ * your Chrome installation on your operating system.
+ */
+
+const exePath =
+  process.platform === "win32"
+    ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    : process.platform === "linux"
+    ? "/usr/bin/google-chrome"
+    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+
+async function getOptions(isDev) {
+  let options;
+  if (isDev) {
+    options = {
+      args: [],
+      executablePath: exePath,
+      headless: true,
+    };
+  } else {
+    options = {
+      args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+      ignoreHTTPSErrors: true,
+    };
   }
-  return true;
+  return options;
 }
 
-export async function getScreenshot(url, ratio = 1) {
-  const page = await getPage();
-  await page.goto(url, {
-    waitUntil: 'domcontentloaded',
-  });
-  await page.setViewport({
-    width: 1000,
-    height: 600,
-    devicePixelRatio: ratio,
-  });
-  const file = await page.screenshot();
-  return file;
+async function logError(err) {
+    console.log('❌ Ocorreu algum erro >', `[${err}]`);
+}
+
+async function openSite(parameters, page) {
+
+    console.log('👉🏻 Abrindo site');
+
+    const userLoginField = '#user_login';
+    const userPassField = '#user_pass';
+    const submitButton = '#wp-submit';
+
+  try {
+
+    try {
+      await page.goto(parameters.url);
+      await page.waitForSelector(userLoginField, { visible: true });
+      await page.waitForSelector(userPassField, { visible: true });
+
+    } catch (error) {
+      await logError(`❌ Não conseguiu acessar a URL: ${error}`);
+
+      try {
+        console.log('👉🏻Tentando url com WP-ADMIN');
+    
+        if (!parameters.url.includes('wp-admin')) {
+          if (parameters.url.slice(-1) === '/') {
+            parameters.url = parameters.url + 'wp-admin';
+          } else {
+            parameters.url = parameters.url + '/wp-admin';
+          }
+        }
+                    
+        await page.goto(parameters.url);
+        await page.waitForSelector(userLoginField, { visible: true });
+        await page.waitForSelector(userPassField, { visible: true });
+    
+      } catch (error) {
+        await logError(`❌ Não conseguiu acessar a URL com WP-Admin`);
+                
+    
+        console.log('👉🏻 Ativação não realizada');
+                
+        await browser.close();
+        throw new Error('❌ Não conseguiu acessar a URL com WP-Admin');
+
+      }
+            
+    }
+
+        
+    try {
+      await page.type(userLoginField, parameters.user);
+      await page.type(userPassField, parameters.password);
+      await page.click(submitButton);
+
+      let loginError = await page.$('#login_error');
+
+      if (loginError) {
+                
+        console.log('👉🏻 Ativação não realizada');
+                
+        await browser.close();
+
+      } else {
+        console.log('👉🏻 Fez login');
+      }
+
+    } catch (e) {
+      console.log('error', e);
+    }
+
+        
+  } catch (e) {
+    console.log('error', e);
+  }
+    
 }
 
 module.exports = async (req, res) => {
-  if (!req.query.url) return res.status(400).send('No url query specified.');
-  if (!checkUrl(req.query.url))
-    return res.status(400).send('Invalid url query specified.');
+  const pageToScreenshot = req.query.page;
+
+  // pass in this parameter if you are developing locally
+  // to ensure puppeteer picks up your machine installation of
+  // Chrome via the configurable options
+  const isDev = req.query.isDev === "true";
+
   try {
-    const file = await getScreenshot(req.query.url, req.query.ratio);
-    res.setHeader('Content-Type', 'image/png');
-    res.setHeader(
-      'Cache-Control',
-      'public, immutable, no-transform, s-maxage=604800, max-age=604800'
-    );
-    res.status(200).end(file);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send(
-        'The server encountered an error. You may have inputted an invalid query.'
-      );
+    // check for https for safety!
+    if (!pageToScreenshot.includes("https://")) {
+      res.statusCode = 404;
+      res.json({
+        body: "Sorry, we couldn't screenshot that page. Did you include https://?",
+      });
+    }
+
+    // get options for browser
+    const options = await getOptions(isDev);
+
+    // launch a new headless browser with dev / prod options
+    const browser = await puppeteer.launch(options);
+    const page = await browser.newPage();
+
+    // set the viewport size
+    await page.setViewport({
+      width: 1920,
+      height: 1080,
+      deviceScaleFactor: 1,
+    });
+
+    // tell the page to visit the url
+    await page.goto(pageToScreenshot);
+
+    const parameters = {
+      url: pageToScreenshot,
+      user: 'voltsstudio',
+      password: 'vs321#@!'
+    }
+    // testing
+    //await openSite(parameters, page);
+
+
+    //take a screenshot
+    const file = await page.screenshot({
+      type: "png",
+    });
+
+    // close the browser
+    await browser.close();
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", `image/png`);
+
+    // return the file!
+    res.end(file);
+  } catch (e) {
+    res.statusCode = 500;
+    res.json({
+      body: "Sorry, Something went wrong!",
+    });
   }
 };
